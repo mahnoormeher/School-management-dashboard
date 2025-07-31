41fc417c54a39b26d47ca452a8e65665db92e567
Mahnoor Meher <mahnoorneher@gmail.com>
2025-08-01 01:39:28 +0500

    Initial commit

--- src/app/(dashboard)/list/assignments/page.tsx
+++ src/app/(dashboard)/list/assignments/page.tsx
@@ -4,11 +4,11 @@ import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
 import prisma from "@/lib/prisma";
 import { ITEM_PER_PAGE } from "@/lib/settings";
-import { currentUserId, role } from "@/lib/utils";
 import { Assignment, Class, Prisma, Subject, Teacher } from "@prisma/client";
 import Image from "next/image";
+import { auth } from "@clerk/nextjs/server";
 
-type AssignmentList=Assignment  & {
+type AssignmentList = Assignment & {
   lesson: {
     subject: Subject;
     class: Class;
@@ -16,45 +16,62 @@ type AssignmentList=Assignment  & {
   };
 };
 
-const columns = [
-  {
-    header: "Subject Name",
-    accessor: "name",
-  },
-  {
-    header: "Class",
-    accessor: "class",
-  },
-  {
-    header: "Teacher",
-    accessor: "teacher",
-    className: "hidden md:table-cell",
-  },
-  {
-    header: "Due Date",
-    accessor: "dueDate",
-    className: "hidden md:table-cell",
-  },
-  ...(role==="admin" || role==="teacher" ? [{
-    header: "Actions",
-    accessor: "action",
-  },
-]:[])
-];
+const AssignmentListPage = async ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) => {
 
- const renderRow = (item:AssignmentList) => (
+  const { userId, sessionClaims } = await auth();
+  const role = (sessionClaims?.metadata as { role?: string })?.role;
+  const currentUserId = userId;
+  
+  
+  const columns = [
+    {
+      header: "Subject Name",
+      accessor: "name",
+    },
+    {
+      header: "Class",
+      accessor: "class",
+    },
+    {
+      header: "Teacher",
+      accessor: "teacher",
+      className: "hidden md:table-cell",
+    },
+    {
+      header: "Due Date",
+      accessor: "dueDate",
+      className: "hidden md:table-cell",
+    },
+    ...(role === "admin" || role === "teacher"
+      ? [
+          {
+            header: "Actions",
+            accessor: "action",
+          },
+        ]
+      : []),
+  ];
+  
+  const renderRow = (item: AssignmentList) => (
     <tr
       key={item.id}
       className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-purple-50"
     >
       <td className="flex items-center gap-4 p-4">{item.lesson.subject.name}</td>
       <td>{item.lesson.class.name}</td>
-      <td className="hidden md:table-cell">{item.lesson.teacher.name + " " +item.lesson.teacher.surname}</td>
-          <td className="hidden md:table-cell"> {new Intl.DateTimeFormat("en-US").format(item.dueDate)}</td>
+      <td className="hidden md:table-cell">
+        {item.lesson.teacher.name + " " + item.lesson.teacher.surname}
+      </td>
+      <td className="hidden md:table-cell">
+        {new Intl.DateTimeFormat("en-US").format(item.dueDate)}
+      </td>
       <td>
         <div className="flex items-center gap-2">
-      
-          {(role=== "admin" || role==="teacher") && (
+          {(role === "admin" || role === "teacher") && (
             <>
               <FormModal table="assignment" type="update" data={item} />
               <FormModal table="assignment" type="delete" id={item.id} />
@@ -65,47 +82,41 @@ const columns = [
     </tr>
   );
 
-const AssignmentListPage = async ({
- searchParams,
-}: {
-  searchParams: { [key: string]: string | undefined };
-}) => {
-  //console.log(searchParams);
-const { page , ...queryParams} = searchParams;
+  const { page, ...queryParams } = searchParams;
 
-const p= page ? parseInt(page) : 1;
+  const p = page ? parseInt(page) : 1;
 
-//URL PARAMS CONDITION
+  // URL PARAMS CONDITION
 
-const query:Prisma.AssignmentWhereInput = {};
+  const query: Prisma.AssignmentWhereInput = {};
 
-query.lesson={};
-if(queryParams){
-  for(const [key,value] of Object.entries(queryParams)){
-    if(value !== undefined){
-    switch(key){
-        case "teacherId":
-          query.lesson.teacherId = value;
-          break;
-           case "classId":
-          query.lesson.classId =parseInt(value);
-          break;
-           case "search":
-          query.lesson.subject={
-              name:{contains:value,mode:"insensitive"},
-            };
-          break;
-            default:
+  query.lesson = {};
+
+  if (queryParams) {
+    for (const [key, value] of Object.entries(queryParams)) {
+      if (value !== undefined) {
+        switch (key) {
+          case "classId":
+            query.lesson.classId = parseInt(value);
             break;
-    }
+          case "teacherId":
+            query.lesson.teacherId = value;
+            break;
+          case "search":
+            query.lesson.subject = {
+              name: { contains: value, mode: "insensitive" },
+            };
+            break;
+          default:
+            break;
+        }
+      }
     }
   }
-}
 
+  // ROLE CONDITIONS
 
-//ROLE CONDITION
-
- switch (role) {
+  switch (role) {
     case "admin":
       break;
     case "teacher":
@@ -133,31 +144,30 @@ if(queryParams){
       break;
   }
 
-const [data, count] = await prisma.$transaction([
-  prisma.assignment.findMany({
-    where: query,
-    include: {
-      lesson:{
-       select:{
-        subject:{select:{name:true}},
-        teacher:{select:{name:true,surname:true}},
-        class:{select:{name:true}},
-       }
-      }
-    },
-    take: ITEM_PER_PAGE,
-    skip: ITEM_PER_PAGE * (p - 1),
-  }),
-  prisma.assignment.count({ where: query }),
-]);
- 
- 
-
+  const [data, count] = await prisma.$transaction([
+    prisma.assignment.findMany({
+      where: query,
+      include: {
+        lesson: {
+          select: {
+            subject: { select: { name: true } },
+            teacher: { select: { name: true, surname: true } },
+            class: { select: { name: true } },
+          },
+        },
+      },
+      take: ITEM_PER_PAGE,
+      skip: ITEM_PER_PAGE * (p - 1),
+    }),
+    prisma.assignment.count({ where: query }),
+  ]);
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/* TOP */}
       <div className="flex items-center justify-between">
-        <h1 className="hidden md:block text-lg font-semibold">All Assignments</h1>
+        <h1 className="hidden md:block text-lg font-semibold">
+          All Assignments
+        </h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
@@ -167,7 +177,10 @@ const [data, count] = await prisma.$transaction([
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow-200">
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
-            {role === "admin" && <FormModal table="assignment" type="create" />}
+            {role === "admin" ||
+              (role === "teacher" && (
+                <FormModal table="assignment" type="create" />
+              ))}
           </div>
         </div>
       </div>
--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -1,26 +1,37 @@
+import Announcements from "@/components/Anouncements";
+import BigCalendarContainer from "@/components/BigCalendarContainer";
+import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
 
 
-import Anouncements from "@/components/Anouncements";
-import BigCalendar from "@/components/BigCalendar";
+const ParentPage = async () => {
+  const { userId } =await  auth();
+  const currentUserId = userId;
+  
+  const students = await prisma.student.findMany({
+    where: {
+      parentId: currentUserId!,
+    },
+  });
 
-import "react-big-calendar/lib/css/react-big-calendar.css";
-
-const ParentPage = () => {
   return (
-    <div className="p-4 flex flex-col gap-4 h-full">
-      <div className="flex flex-col lg:flex-row gap-4 h-full">
-        
-        {/* LEFT */}
-        <div className="flex-1 w-full lg:w-2/3 bg-white p-4 rounded-md h-[770px]">
-          <h1 className="text-xl font-semibold mb-4">Schedule (Mahnoor)</h1>
-          <BigCalendar />
-        </div>
-
-        {/* RIGHT */}
-        <div className="w-full lg:w-1/3 flex flex-col gap-4">
-          
-          <Anouncements />
-        </div>
+    <div className="flex-1 p-4 flex gap-4 flex-col xl:flex-row">
+      {/* LEFT */}
+      <div className="">
+        {students.map((student) => (
+          <div className="w-full xl:w-2/3" key={student.id}>
+            <div className="h-full bg-white p-4 rounded-md">
+              <h1 className="text-xl font-semibold">
+                Schedule ({student.name + " " + student.surname})
+              </h1>
+              <BigCalendarContainer type="classId" id={student.classId} />
+            </div>
+          </div>
+        ))}
+      </div>
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <Announcements />
       </div>
     </div>
   );
